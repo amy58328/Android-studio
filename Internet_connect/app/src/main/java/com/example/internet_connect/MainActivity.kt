ba21414dcd0ba61ae53bd6a72fcae37ae2688203
Una <amy58328@gmail.com>
2019-12-18 01:49:04 +0800

    Una 12/18 1

--- Internet_connect/app/src/main/java/com/example/internet_connect/MainActivity.kt
+++ Internet_connect/app/src/main/java/com/example/internet_connect/MainActivity.kt
@@ -7,42 +7,51 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.RequestQueue
 import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.w3c.dom.Text
+import kotlin.math.log
 
 
 //import javax.swing.UIManager.put
 
 
 class MainActivity : AppCompatActivity() {
-    private var objqueue:RequestQueue? = null
-    private  var getRequest:StringRequest? = null
+    private var jsonArray:JsonArrayRequest?=null
     private  val strurl = "http://140.136.149.224:3000/subject"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        newrequest()
+
+        new_request()
    }
 
-    fun newrequest()
-    {
-        var lbl_result = findViewById(R.id.post_text) as TextView
-        objqueue = Volley.newRequestQueue(this)
-        getRequest =
-            StringRequest(strurl, Response.Listener { response ->
-                //response，表示是回傳值，就是API要回傳的字串，也可以是JSON字串。
-                lbl_result.text = response
-            }, Response.ErrorListener { error ->
-                //如果發生錯誤，就是回傳VolleyError，可以顯示是什麼錯誤。
-                lbl_result.text = error.message
-            })
+    fun new_request(){
+        var post_text = findViewById(R.id.post_text) as TextView
+        jsonArray = JsonArrayRequest(strurl,
+            object: Response.Listener<JSONArray> {
+                override fun onResponse(response: JSONArray?) {
+                    post_text.text = response.toString()
+
+                }
+            },
+            object : Response.ErrorListener{
+                override fun onErrorResponse(error: VolleyError?) {
+                    if (error != null) {
+                        post_text.text = error.message
+                    }
+                }
+            }
+        )
     }
 
-
     fun prc_getmessage(v: View?) { //將getRequest物件加入Volley物件的queue中，執行跟API的溝通。
-        objqueue!!.add(getRequest)
-        newrequest()
+        Volley.newRequestQueue(this).add(jsonArray)
+        new_request()
     }
 
 }
